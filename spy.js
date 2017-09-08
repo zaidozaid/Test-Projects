93b7ae0c03e56dee6dfde0ef6d62003b6a599b7e
zaidozaid <zaidozaid@gmail.com>
2017-09-08 22:34:22 +0530

    comit
    Comit

---
+++ spy.js
@@ -0,0 +1,14 @@
+ var firstname = prompt("Heloo and Welcome. Please enter your first name:")
+ var lastname = prompt("Please enter your Last Name:")
+ var age = prompt("How old are you?")
+ var height = prompt("How tall are you in centimeters")
+ var pet = prompt("What is the name of your pet?")
+
+ if(firstname[0]==lastname[0] && age > 20 && age < 30 && height >= 170 && pet[-0] == 'y') {
+
+   console.log("you are a Spy!")
+
+ }
+ else {
+   console.log("Sorry there is nothing for you!");
+ }
