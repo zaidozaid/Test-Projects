a849492fbe5c2877c4cc4fdcda28fa1134ce6cc1
zaidozaid <zaidozaid@gmail.com>
2017-09-20 17:35:36 +0530

    Commit

--- connectfour.js
+++ connectfour.js
@@ -2,4 +2,16 @@
  * Created by Zaid.Bhat on 9/14/2017.
  */
 
+function changecolor(rowIndex,colIndex,color) {
+    return table.eq(rowIndex).find('td').eq(colIndex).find('button').css('background-color',color);
+}
 
+function returncolor(rowIndex,colIndex) {
+    return table.eq(rowIndex).find('td').eq(colIndex).find('button').css('background-color');
+}
+
+function checkBottom(collIndex) {
+
+
+
+}
